5a8cd903893d4e9cee85fea077795465157d8143
Juan Navas <juannavas7@gmail.com>
2018-03-14 19:16:41 +0100

    Initial Commit

---
+++ LPNotesManagerCodeTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LPNotesManagerCodeTest
+//
+//  Created by Juan Navas Martin on 14/3/18.
+//  Copyright © 2018 Perhapps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
